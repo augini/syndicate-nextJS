757f8c1834f5900536c945249f4373d4dc7308ba
Farrukh Atabekov <augini18@gmail.com>
2020-11-14 16:05:58 +0900

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const name = () => {
+    console.log("object");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,8 +19,8 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing{" "}
+          <code className={styles.code}>pafsdfges/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -56,10 +60,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
